c2322d25b3945a66cdf1a4d6aa07ebb3b6d17745
pallat <yod.pallat@gmail.com>
2018-07-12 22:03:05 +0700

    fix race condition

--- robin.go
+++ robin.go
@@ -1,18 +1,29 @@
 package round
 
+import (
+	"sync"
+)
+
 func NewRoundRobin(n int) func() <-chan int {
-	seq := []int{}
 	cur := 0
 	ch := make(chan int, 1)
+	seq := makeSeq(n)
+	mux := sync.Mutex{}
 
+	return func() <-chan int {
+		mux.Lock()
+		ch <- cur
+		cur = seq[cur]
+		mux.Unlock()
+		return ch
+	}
+}
+
+func makeSeq(n int) []int {
+	seq := []int{}
 	for i := 0; i < (n - 1); i++ {
 		seq = append(seq, i+1)
 	}
 	seq = append(seq, 0)
-
-	return func() <-chan int {
-		ch <- cur
-		cur = seq[cur]
-		return ch
-	}
+	return seq
 }
--- robin_test.go
+++ robin_test.go
@@ -1,8 +1,18 @@
 package round
 
 import (
+	"reflect"
+	"sync"
 	"testing"
 )
+
+func TestMakeSequence(t *testing.T) {
+	seq := makeSeq(5)
+	expected := []int{1, 2, 3, 4, 0}
+	if !reflect.DeepEqual(expected, seq) {
+		t.Fatalf("expected=%v\nseq%v\n", expected, seq)
+	}
+}
 
 func TestRoundRobin(t *testing.T) {
 	rr := NewRoundRobin(10)
@@ -16,5 +26,48 @@ func TestRoundRobin(t *testing.T) {
 		if n := <-rr(); n != i {
 			t.Fatalf("it should return seuence number 0..4 but at index %d got %d", i, n)
 		}
+	}
+}
+
+func TestRoundRobinHitByConcurrency(t *testing.T) {
+	rr := NewRoundRobin(10)
+
+	var a, b, c int
+	wg := sync.WaitGroup{}
+	wg.Add(30)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			a += <-rr()
+			wg.Done()
+		}
+	}()
+	go func() {
+		for i := 0; i < 10; i++ {
+			b += <-rr()
+			wg.Done()
+		}
+	}()
+	go func() {
+		for i := 0; i < 10; i++ {
+			c += <-rr()
+			wg.Done()
+		}
+	}()
+
+	wg.Wait()
+
+	sum := a + b + c
+
+	if 135 != sum {
+		t.Errorf("lost some\n(a=%d)\n (b=%d)\n (c=%d)\n", a, b, c)
+	}
+}
+
+func BenchmarkRR(b *testing.B) {
+	rr := NewRoundRobin(10)
+
+	for i := 0; i < b.N; i++ {
+		<-rr()
 	}
 }
